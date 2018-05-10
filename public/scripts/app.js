ba2981f88c424544f48d26a3a130671c59bb7099
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-10 21:22:47 +0200

    ES6 Aside: Arrow Functions - PART I

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,81 +1,13 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running');
-
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+var square = function square(x) {
+    return x * x;
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No option'
-    ),
-    React.createElement(
-        'ul',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+// console.log(square(8));
 
-var user = {
-    name: 'Yuki',
-    age: 27,
-    location: 'Brussels'
+var squareArrow = function squareArrow(x) {
+    return x * x;
 };
 
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-}
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(squareArrow(5));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,11 @@
+const square = function(x) {
+    return x * x;
+};
+
+// console.log(square(8));
+
+const squareArrow = (x) => {
+    return x * x
+};
+
+console.log(squareArrow(5));
