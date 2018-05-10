ca476086bf18bc2520e9ba3f812d21f5358552fd
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-10 21:44:28 +0200

    ES6 Aside: Arrow Functions - PART II - Challange - getFirstName

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,38 @@
-"use strict";
+'use strict';
 
-function square(x) {
-    return x * x;
+// function square(x) {
+//     return x * x;
+// };
+
+// console.log(square(3));
+
+// // const squareArrow = (x) => {
+// //     return x * x
+// // };
+
+
+// // Another expression syntax
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+
+// CHALLANGE - Use Arrow Function
+// getFirstName ('Mike Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using short hand syntax
+
+/* FIRST VERSION
+const getFirstName = (fullName) => {
+    return fullName.split(' ')[0];
+}
+
+console.log(getFirstName('Yudha Yuki'));
+*/
+
+// SECOND VERSION
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(' ')[0];
 };
 
-console.log(square(3));
-
-var squareArrow = function squareArrow(x) {
-    return x * x;
-};
-
-console.log(squareArrow(5));
+console.log(getFirstName('Yudha Yuki'));
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,11 +1,39 @@
-function square(x) {
-    return x * x;
-};
+// function square(x) {
+//     return x * x;
+// };
 
-console.log(square(3));
+// console.log(square(3));
 
-const squareArrow = (x) => {
-    return x * x
-};
+// // const squareArrow = (x) => {
+// //     return x * x
+// // };
 
-console.log(squareArrow(5));
+
+// // Another expression syntax
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+
+
+
+
+// CHALLANGE - Use Arrow Function
+// getFirstName ('Mike Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using short hand syntax
+
+/* FIRST VERSION
+const getFirstName = (fullName) => {
+    return fullName.split(' ')[0];
+}
+
+console.log(getFirstName('Yudha Yuki'));
+*/
+
+
+// SECOND VERSION
+const getFirstName = fullName => fullName.split(' ')[0];
+
+console.log(getFirstName('Yudha Yuki'));
+
