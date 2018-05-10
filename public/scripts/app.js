6a755ac1b58f11640f1ccdfeb52556202c5cc08c
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-10 22:19:00 +0200

    ES6 Aside: Arrow Functions - PART III - THIS keywords in function  ES5 vs ES6

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Arguments object - no longer bound with arrow function
 
@@ -12,12 +12,42 @@ const add = function(a, b) {
 */
 
 // ES6
-
-var add = function add(a, b) {
+/*
+const add = (a, b) => {
     // console.log(arguments);
     return a + b;
-};
-
-console.log(add(55, 1, 1001));
+}
+ console.log(add(55, 1, 1001))
+*/
 
 // this keyword - no longer bound
+
+// ES5
+/*
+const user = {
+    name: 'Yudha',
+    cities: ['Brussels', 'Prague', 'Rotterdam'],
+    printPlacesLived : function () {
+        const that = this;
+         this.cities.forEach(function(city) {
+            console.log(that.name + ' has lived in ' + city)
+        })
+    }
+};
+ user.printPlacesLived();
+*/
+
+// ES6
+var user = {
+    name: 'Yudha',
+    cities: ['Brussels', 'Prague', 'Rotterdam'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        this.cities.forEach(function (city) {
+            console.log(_this.name + ' has lived in ' + city);
+        });
+    }
+};
+
+user.printPlacesLived();
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -11,19 +11,48 @@
         */
 
         // ES6
-        
+        /*
         const add = (a, b) => {
             // console.log(arguments);
             return a + b;
         }
 
         console.log(add(55, 1, 1001))
-        
+        */
 
 
 
 // this keyword - no longer bound
 
+        // ES5
+        /*
+        const user = {
+            name: 'Yudha',
+            cities: ['Brussels', 'Prague', 'Rotterdam'],
+            printPlacesLived : function () {
+                const that = this;
 
+                this.cities.forEach(function(city) {
+                    console.log(that.name + ' has lived in ' + city)
+                })
+            }
+        };
+
+        user.printPlacesLived();
+        */
+
+
+        // ES6
+        const user = {
+            name: 'Yudha',
+            cities: ['Brussels', 'Prague', 'Rotterdam'],
+            printPlacesLived : function () {
+                this.cities.forEach((city) => {
+                    console.log(this.name + ' has lived in ' + city)
+                })
+            }
+        };
+
+        user.printPlacesLived();
 
 
