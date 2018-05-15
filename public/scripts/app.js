1e57f33de095963b291b2f5b3404403fb610a382
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-15 15:17:19 +0200

    Forms and inputs - Attach PREVENTDEFAULT method for not reloading the page when we submit things

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,12 @@ var app = {
     options: ['One', 'Two']
 };
 
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log('Form submitted');
+};
+
 var template = React.createElement(
     'div',
     null,
@@ -27,7 +33,7 @@ var template = React.createElement(
         app.options.length > 0 ? 'Here are your options' : 'No option'
     ),
     React.createElement(
-        'ul',
+        'ol',
         null,
         React.createElement(
             'li',
@@ -39,56 +45,19 @@ var template = React.createElement(
             null,
             'Item two'
         )
+    ),
+    React.createElement(
+        'form',
+        { onSubmit: onFormSubmit },
+        React.createElement('input', { type: 'text', name: 'option' }),
+        React.createElement(
+            'button',
+            null,
+            'Add Option'
+        )
     )
 );
 
-var count = 0;
-
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
-};
-
 var appRoot = document.getElementById('app');
 
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Count: ',
-            count
-        ),
-        React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'Reset'
-        )
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -6,50 +6,29 @@ const app = {
     options: ['One', 'Two']
 }
 
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form submitted');
+}
+
 const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? 'Here are your options' : 'No option'}</p>
-        <ul>
+        <ol>
             <li>Item one</li>
             <li>Item two</li>
-        </ul>
+        </ol>
+
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+        </form>
     </div> 
 );
 
-
-let count = 0;
-
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
-
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div> 
-            <h1>Count: {count}</h1>
-            <button onClick ={addOne}>+1</button>
-            <button onClick ={minusOne}>-1</button>
-            <button onClick ={reset}>Reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-}
-
-renderCounterApp();
-
+ReactDOM.render(template, appRoot);
