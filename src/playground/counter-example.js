802d9d0610f81c008c444ca7f7a44f34f5788441
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-17 12:58:07 +0200

    Adding State to Counter App: PART I

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,3 +1,55 @@
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    handleAddOne() {
+        console.log('handleAddOne');
+    }
+
+    handleMinusOne() {
+        console.log('handleMinusOne');
+    }
+
+    handleReset() {
+        console.log('handleReset');
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <h1>Count: </h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        );
+    }
+
+
+
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+
+// CHALLANGE
+/*
+    - Create THREE METHOD: handleAddOne, handleMinusOne, handleReset
+    - Use console.log to print method name
+    - Wire Up onClick & bind in the contructor
+*/
+
+
+
+
+
+/* 
 let count = 0;
 
 const addOne = () => {
@@ -31,3 +83,4 @@ const renderCounterApp = () => {
 }
 
 renderCounterApp();
+*/
