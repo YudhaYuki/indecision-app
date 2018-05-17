8924073b86bd7508dd661b6df97d169ba9303d8a
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-17 17:12:28 +0200

    Indecision State: PATI II - Form Validation

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: ['Thing one', 'Thing two', 'Thing three']
+            options: []
         };
     }
 
@@ -24,6 +24,13 @@ class IndecisionApp extends React.Component {
     }
 
     handleAddOption(option) {
+
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exist';
+        }
+
         this.setState((prevState) => {
             return {
                 options: prevState.options.concat(option)
@@ -110,21 +117,26 @@ class AddOption extends React.Component {
     constructor(props) {
         super(props);
         this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
     }
 
     handleAddOption(e) {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
 
-        if (option) {
-            this.props.handleAddOption(option);
-        }
+        this.setState(() => {
+            return { error };
+        });
     } 
 
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button>Submit</button>
