336732449e900711e9a9c13cda0bdeeb463198d1
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-17 13:30:56 +0200

    Adding State to Counter App: PART II : Decrement VALU by 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,8 +20,7 @@ var Counter = function (_React$Component) {
         _this.handleMinusOne = _this.handleMinusOne.bind(_this);
         _this.handleReset = _this.handleReset.bind(_this);
         _this.state = {
-            count: 0,
-            name: 'Yudha'
+            count: 0
         };
         return _this;
     }
@@ -38,7 +37,11 @@ var Counter = function (_React$Component) {
     }, {
         key: 'handleMinusOne',
         value: function handleMinusOne() {
-            console.log('handleMinusOne');
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count - 1
+                };
+            });
         }
     }, {
         key: 'handleReset',
@@ -51,7 +54,6 @@ var Counter = function (_React$Component) {
             return React.createElement(
                 'div',
                 null,
-                this.state.name,
                 React.createElement(
                     'h1',
                     null,
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -6,8 +6,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: 0,
-            name: 'Yudha'
+            count: 0
         };
     }
 
@@ -20,7 +19,11 @@ class Counter extends React.Component {
     }
 
     handleMinusOne() {
-        console.log('handleMinusOne');
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        });
     }
 
     handleReset() {
@@ -31,7 +34,6 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
-                {this.state.name}
                 <h1>Count: {this.state.count} </h1>
                 <button onClick={this.handleAddOne}>+1</button>
                 <button onClick={this.handleMinusOne}>-1</button>
