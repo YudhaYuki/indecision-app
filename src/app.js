3ab5625ca67afc35f09170b44bf94a54f741a697
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-17 08:34:28 +0200

    Creating a React Component - CHALLANGE add two other components

--- src/app.js
+++ src/app.js
@@ -13,7 +13,27 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button> What should I do ?</button>
+                <button>What should I do ?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options component here
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                Add option component here
             </div>
         );
     }
@@ -23,6 +43,8 @@ const jsx = (
     <div>
         <Header />
         <Action />
+        <Options />
+        <AddOption />
     </div>
 );
 
