8bc5134dfeebfeffb22c8a104e383c69b497780a
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-21 15:59:43 +0200

    Importing NPM Modules - Install react and react dom, and try to use it (ERROR occurs)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // install -> import -> use
 
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(validator.isEmail('test@test.com'));
+const template = <p>Testing 123</p>;
+ReactDOM.render(template, document.getElementById('app'));
