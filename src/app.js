6f44d225aebb8e3872fcf27396a5b796221c512c
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-21 15:40:16 +0200

    Importing NPM Modules - Install validator - use one of the content inside it named VALIDATOR.EMAIL - Ex.1

--- src/app.js
+++ src/app.js
@@ -1,42 +1,5 @@
-// import './utils.js';
-// import substract, { square, add } from './utils.js';
+// install -> import -> use
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(substract(100, 20));
+import validator from 'validator';
 
-
-
-
-
-
-// CHALLANGE
-/*
-    - Create Person.js
-    - Named export isAdult(18) - true if adult, otherwise false
-    - Named export canDrink(18) - true if 21 and over, otherwise false
-
-    // import isAdult and canDrink
-    // Use both printing result to the console
-*/
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(18));
-console.log(canDrink(20));
-
-
-
-
-
-
-
-
-// CHALLANGE II
-/*
-    - Setting the default export and function
-    - Grab the default and call it
-*/
-
-console.log(isSenior(65));
+console.log(validator.isEmail('test@test.com'));
