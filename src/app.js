bb24a992324dc90229b088455f227b14abfb96ac
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-22 08:06:14 +0200

    ES6 class properties - Try to iplement NEW SYNTAX in AddOption file

--- src/app.js
+++ src/app.js
@@ -3,3 +3,26 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+// -------- CLASS OLD SYNTAX
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Mike';
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+
+
+// -------- CLASS OLD SYNTAX
+
+class NewSyntax {
+    name = 'Jane';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
