7776796892bbce67113f9a256ff95d1a1505264e
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-22 08:17:53 +0200

    ES6 class properties - Try to iplement NEW SYNTAX in AddOption file NOT USING BIND

--- src/app.js
+++ src/app.js
@@ -6,23 +6,30 @@ ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
 
 // -------- CLASS OLD SYNTAX
-
 class OldSyntax {
     constructor() {
         this.name = 'Mike';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+    getGreeting() {
+        return `Hi, my name is ${this.name}.`;
     }
 }
 
 const oldSyntax = new OldSyntax();
-console.log(oldSyntax);
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
 
 
 
 // -------- CLASS OLD SYNTAX
-
 class NewSyntax {
     name = 'Jane';
+    getGreeting = () => {
+        return `Hi, my name is ${this.name}.`;
+    }
 }
 
 const newSyntax = new NewSyntax();
-console.log(newSyntax);
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-
     state = {
         error: undefined
-    }
+    };
 
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-    }
-
-    handleAddOption(e) {
+    handleAddOption = (e) => {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim();
